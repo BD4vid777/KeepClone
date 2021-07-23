82f3e2d3f9930feab694c15b3253e4c7a77cedf0
Dawid <64140934+BD4vid777@users.noreply.github.com>
2021-07-23 23:33:34 +0200

    Deleting Note

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ class App {
             this.handleFormClick(event);
             this.selectNote(event);
             this.openModal(event);
+            this.deleteNote(event);
         });
 
         document.body.addEventListener('mouseover', event => {
@@ -103,6 +104,7 @@ class App {
     }
 
     openModal(event) {
+        if (event.target.matches('.toolbar-delete')) return;
         if (event.target.closest('.note')) {
             this.$modal.classList.toggle('open-modal');
             this.$modalTitle.value = this.title;
@@ -167,6 +169,14 @@ class App {
         this.id = $selectedNote.dataset.id;
     }
 
+    deleteNote(event) {
+        event.stopPropagation();
+        if (!event.target.matches('.toolbar-delete')) return;
+        const id = event.target.dataset.id;
+        this.notes = this.notes.filter(note => note.id !== Number(id));
+        this.displayNotes();
+    }
+
     displayNotes() {
         const hasNotes = this.notes.length > 0;
         this.$placeholder.style.display = hasNotes ? "none" : "flex";
@@ -180,7 +190,7 @@ class App {
           <div class="toolbar-container">
             <div class="toolbar">
               <img class="toolbar-color" data-id=${note.id} src="https://img.icons8.com/material-outlined/24/000000/paint-palette.png"/>
-              <img class="toolbar-delete" src="https://img.icons8.com/material-outlined/24/000000/delete-forever.png"/>
+              <img class="toolbar-delete" data-id=${note.id} src="https://img.icons8.com/material-outlined/24/000000/delete-forever.png"/>
             </div>
           </div>
         </div>
